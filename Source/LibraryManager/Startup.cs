08431e4ce5de595e93887e637d66c0102d69f72e
Christian Bilotta <christian.bilotta28@gmail.com>
2021-10-29 07:07:51 +0200

    Swagger path to work after site publish

--- Source/LibraryManager/Startup.cs
+++ Source/LibraryManager/Startup.cs
@@ -113,7 +113,7 @@ namespace LibraryManager
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LibraryManager v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("../swagger/v1/swagger.json", "LibraryManager v1"));
             }
 
             app.UseHttpsRedirection();
