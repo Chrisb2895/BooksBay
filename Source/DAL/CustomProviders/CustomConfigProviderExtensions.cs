5b7450b36ba86883aab92a805c538a9234f7ad81
Christian Bilotta <christian.bilotta28@gmail.com>
2022-04-29 07:25:24 +0200

    NET 6 First Add Utils Classes for decrypt configuration Build OK

---
+++ Source/DAL/CustomProviders/CustomConfigProviderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DAL.CustomProviders
+{
+    public static class CustomConfigProviderExtensions
+    {
+        public static IConfigurationBuilder AddEncryptedProvider(this IConfigurationBuilder builder, IConfiguration configuration)
+        {
+            return builder.Add(new CustomConfigProvider(configuration));
+        }
+    }
+}
